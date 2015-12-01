e7ca133f032b091da2a9ad89c854e5b7ed10e37d
Trevor Frese <trevor.frese@gmail.com>
2015-11-30 17:10:22 -0800

    adds functionality for sending messages and checking if a api token is valid and secret file for api token

--- lib/slack_it.rb
+++ lib/slack_it.rb
@@ -1,4 +1,36 @@
-require "slack_it/version"
+# require "slack_it/version"
+require 'net/http'
+require 'envyable'
+require 'json'
+Envyable.load('../config/env.yml', 'slack')
+
+SLACK_API_TOKEN = ENV['API_TOKEN']
+
+def api_token_valid?
+  url = URI.parse("https://slack.com/api/auth.test?token=#{SLACK_API_TOKEN}&pretty=1")
+
+  request = Net::HTTP::Get.new(url.to_s)
+  response = Net::HTTP.start(url.host, url.port, :use_ssl => url.scheme == 'https') {|http|
+    http.request(request)
+  }
+  return JSON.parse(response.body)['ok']
+end
+
+
+
+puts api_token_valid?
+
+def send_message(message, channel,as_user=nil)
+  url = URI.parse("https://slack.com/api/chat.postMessage?token=#{SLACK_API_TOKEN}&channel=#{channel}&text=#{message}&as_user=#{as_user}&pretty=1")
+
+  request = Net::HTTP::Get.new(url.to_s)
+  response = Net::HTTP.start(url.host, url.port, :use_ssl => url.scheme == 'https') {|http|
+    http.request(request)
+  }
+  return JSON.parse(response.body)['ok']
+end
+
+puts send_message("somethingelse", "%23testing_slack_it", "trevorfrese")
 
 module SlackIt
   # Your code goes here...

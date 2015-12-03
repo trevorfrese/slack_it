2e42535ce9e32b8afd435536d3dfa8362f3a69a4
Trevor Frese <trevor.frese@gmail.com>
2015-12-02 19:05:32 -0800

    changes methods to be instance methods and adds message sending test

--- spec/slack_it_spec.rb
+++ spec/slack_it_spec.rb
@@ -7,27 +7,33 @@ describe SlackIt do
 
   describe "API token" do
     it 'should not be empty' do
-      expect(api_token_nil_or_empty?("")).to be true
-      expect(api_token_nil_or_empty?(nil)).to be true
-      expect(api_token_nil_or_empty?("asdgasgd")).to be false
+      expect(SlackIt::api_token_nil_or_empty?("")).to be true
+      expect(SlackIt::api_token_nil_or_empty?(nil)).to be true
+      expect(SlackIt::api_token_nil_or_empty?("asdgasgd")).to be false
     end
 
     it 'should be valid' do
       valid_token = "xoxp-2338905229-6461272661-13532622482-47b2552346"
 
-      expect(api_token_valid?("asdgasdg")).to be false
-      expect(api_token_valid?(nil)).to be false
-      expect(api_token_valid?(valid_token)).to be true
+      expect(SlackIt::api_token_valid?("asdgasdg")).to be false
+      expect(SlackIt::api_token_valid?(nil)).to be false
+      expect(SlackIt::api_token_valid?(valid_token)).to be true
     end
 
     it 'should be inserted into a config file correctly' do
-      token = "xoxp-0123456789-0123456789-01234567891-0123456789"
+      token = "xoxp-2338905229-6461272661-13532622482-47b2552346"
 
-      create_config_file(token)
+      SlackIt::create_config_file(token)
       expect(File.exists?("config")).to be true
 
       text = File.open("config/env.yml", "r").read
       expect(text.include?(token)).to be true
+    end
+
+    it 'should send a message to a channel' do
+      result = SlackIt::send_message("testvalue", "%23testing_slack_it", "trevorfrese")
+
+      expect(result).to be true
     end
   end
 end
